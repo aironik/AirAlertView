45ee9f7946502ea3be79a3fd8612441d8b2a30a7
Oleg Lobachev <aironik@gmail.com>
2014-01-13 13:41:14 +0400

    initial commit

---
+++ AirAlertView/AirAlertView.m
@@ -0,0 +1,13 @@
+//
+//  AirAlertView.m
+//  AirAlertView
+//
+//  Created by Oleg Lobachev on 13.01.14.
+//  Copyright (c) 2014 aironik. All rights reserved.
+//
+
+#import "AirAlertView.h"
+
+@implementation AirAlertView
+
+@end
